7d3c15d9d93cbf1274483ef6466ad9f304a26f88
Nikko Abucejo <me@nikkoabucejo.com>
2024-02-09 22:20:05 +0800

    chore: add middleware

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { auth } from "@/core/libraries/auth";
+
+export default auth((request) => {
+  console.log("HELLO");
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/api/hello"],
+};
