d6894df6e086f8bc539a1e9e68392ed349e6106c
Nikko Abucejo <me@nikkoabucejo.com>
2024-02-11 12:01:11 +0800

    chore: update logger

--- core/libraries/logger.ts
+++ core/libraries/logger.ts
@@ -1,5 +1,5 @@
 const logger = {
-  debug: (...args: any) => console.log("\x1b[34mDebug", ...args, "\x1b[0m"),
+  debug: (...args: any) => console.log("\x1b[34mDebug\x1b[0m", ...args),
   success: (...args: any) => console.log("\x1b[32mSuccess\x1b[0m", ...args),
   error: (...args: any) => console.error("\x1b[31mError\x1b[0m", ...args),
 };
