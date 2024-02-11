b125d7575d6c0db6e6560c8c249d10d1c6fb6fe0
Nikko Abucejo <me@nikkoabucejo.com>
2024-02-11 15:58:09 +0800

    chore: remove matcher

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,3 @@
 import auth from "@libraries/auth";
 
 export default auth((request) => {});
-
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)", "/api/hello"],
-};
