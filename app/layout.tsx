3d281195b08670987d33ee7c97baf1a7191fa51f
jazz <jzztn.crz@gmail.com>
2024-02-12 19:00:28 +0800

    chore: Refactor import path for Providers component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
-import Providers from "@libraries/providers";
+import Providers from "../core/components/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
