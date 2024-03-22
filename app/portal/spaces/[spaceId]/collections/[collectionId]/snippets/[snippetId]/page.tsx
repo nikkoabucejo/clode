0a8f9fabb21a64f722624af5b7307daf470ea035
Nikko Abucejo <me@nikkoabucejo.com>
2024-03-22 23:31:09 +0800

    chore: add description

--- app/portal/spaces/[spaceId]/collections/[collectionId]/snippets/[snippetId]/page.tsx
+++ app/portal/spaces/[spaceId]/collections/[collectionId]/snippets/[snippetId]/page.tsx
@@ -9,7 +9,21 @@ const Snippet: Page = () => {
   return (
     <main className="max-h-full space-y-8 overflow-y-auto p-8">
       <Heading />
-      <Editor snippet={decrypted} />
+      <div className="space-y-4">
+        <p className="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi rem
+          doloribus similique ea excepturi, harum cum sapiente corporis error
+          fugit aut doloremque fuga enim vel, iusto explicabo esse illo amet.
+          Deleniti corrupti laborum similique earum modi minus a iure eaque
+          architecto aliquam expedita ipsam cumque qui nemo soluta eligendi,
+          molestias magnam fuga labore sunt, optio quaerat. Minus sit numquam
+          accusantium. Vero, maiores dolores tempore dolore quasi amet quaerat
+          harum ut consectetur repellat, mollitia libero distinctio, at
+          assumenda ad expedita. Laborum voluptatum quis quos incidunt ullam
+          molestias optio aperiam dolorem a!
+        </p>
+        <Editor snippet={decrypted} />
+      </div>
     </main>
   );
 };
