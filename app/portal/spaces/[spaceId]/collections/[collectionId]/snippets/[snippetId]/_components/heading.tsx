9e6da0471e44018531ccf9f42662f8c7b43bc05d
Nikko Abucejo <me@nikkoabucejo.com>
2024-03-23 10:23:18 +0800

    chore: update placement

--- app/portal/spaces/[spaceId]/collections/[collectionId]/snippets/[snippetId]/_components/heading.tsx
+++ app/portal/spaces/[spaceId]/collections/[collectionId]/snippets/[snippetId]/_components/heading.tsx
@@ -17,6 +17,33 @@ type Props = {
 const Heading = ({ space, collection }: Props) => {
   return (
     <nav className="space-y-4">
+      <AvatarGroup className="justify-start " max={10} size="sm">
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
+        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" />
+      </AvatarGroup>
+
       <Breadcrumbs
         itemClasses={{
           separator: "px-2 text-white",
@@ -28,33 +55,6 @@ const Heading = ({ space, collection }: Props) => {
         </BreadcrumbItem>
         <BreadcrumbItem isDisabled>Sample React Snippet</BreadcrumbItem>
       </Breadcrumbs>
-
-      <AvatarGroup isBordered className="justify-start">
-        <Avatar
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-        />
-        <Avatar
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
-        />
-        <Avatar
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-        />
-        <Avatar
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a04258114e29026302d"
-        />
-        <Avatar
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a04258114e29026702d"
-        />
-        <Avatar
-          size="sm"
-          src="https://i.pravatar.cc/150?u=a04258114e29026708c"
-        />
-      </AvatarGroup>
 
       <div className="space-y-2">
         <div className="flex items-center gap-4">
