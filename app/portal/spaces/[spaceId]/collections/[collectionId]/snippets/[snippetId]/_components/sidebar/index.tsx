ed1a9cebf2df7bdc1c71a1ab0ac43f0d7363232c
Nikko Abucejo <me@nikkoabucejo.com>
2024-03-23 10:05:31 +0800

    chore: reduce padding

--- app/portal/spaces/[spaceId]/collections/[collectionId]/snippets/[snippetId]/_components/sidebar/index.tsx
+++ app/portal/spaces/[spaceId]/collections/[collectionId]/snippets/[snippetId]/_components/sidebar/index.tsx
@@ -13,7 +13,7 @@ const Sidebar = async ({ spaceId }: Props) => {
   const user = await api.server.get.user();
 
   return (
-    <aside className="grid h-full w-[280px] grid-rows-[auto,1fr,auto] gap-8 border-r border-line bg-panel-secondary px-4 py-8">
+    <aside className="grid h-full w-[280px] grid-rows-[auto,1fr,auto] gap-8 border-r border-line bg-panel-secondary px-4 pb-4 pt-8">
       <div className="flex items-center">
         <span className="text-xl font-bold">My Space</span>
         <Icon Element={ChevronUpDownIcon} className="ml-auto" />
